e8ad11669991fb57d666e00591e52887ed34b753
smartman1234 <john.smith.dev0724@gmail.com>
2019-02-07 16:15:03 +0000

    Add sth about bluetooth

--- mobile-app/RoomOccupancy/app/src/main/java/com/example/roomoccupancy/MainActivity.java
+++ mobile-app/RoomOccupancy/app/src/main/java/com/example/roomoccupancy/MainActivity.java
@@ -8,12 +8,67 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "BluetoothConnActivity";
+    AlertDialog mSelectionDialog;
+    DevicesAdapter mDevicesAdapter;
+    BluetoothAdapter mBluetoothAdapter;
+    Handler mHandler;
+    boolean mScanning;
+    BluetoothGatt mGatt;
+    BluetoothManager btManager;
+    private static final int SCAN_PERIOD = 10000;
+    private static final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        mHandler = new Handler();
+        // create instances of mHandler and mDevicesAdapter (add code)
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // set dialog title (add code here)
+
+        builder.setAdapter(mDevicesAdapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finishScanning();
+
+                // Connect to GATT server (add code)
+            }
+        });
+
+        builder.setNegativeButton(R.string.cancel, null);
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                finishScanning();
+            }
+        });
+
+        // create an instance of mSelectionDialog (add code)
+
+        // Enable Bluetooth (add code)
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -22,11 +77,108 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Test!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        btManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = btManager.getAdapter();
     }
+
+    public void onConnectClick(View view) {
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        } else {
+            String btnText = ((Button) view).getText().toString();
+
+            if (btnText.equals(getString(R.string.connect))) {
+                openSelectionDialog();
+            } else if (btnText.equals(getString(R.string.disconnect))) {
+                // add code here
+
+                updateConnectButton(BluetoothProfile.STATE_DISCONNECTED);
+            }
+        }
+    }
+
+    void openSelectionDialog() {
+        beginScanning();
+        // show the selection dialog (add code)
+    }
+
+    private void beginScanning() {
+        if (!mScanning) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {//TODO: problems starts from here
+                @Override
+                public void run() {
+                    finishScanning();
+                }
+            }, SCAN_PERIOD);
+
+            // add code here (mDevicesAdapter and mBluetoothAdapter)
+        }
+    }
+
+    private void finishScanning() {
+        if (mScanning) {
+            // add code here
+        }
+    }
+
+    private void updateConnectButton(int state) {
+        // Create the Button (add code)
+        switch (state) {
+            // Change the text of the button
+            case BluetoothProfile.STATE_DISCONNECTED:
+                // add code here
+                break;
+            case BluetoothProfile.STATE_CONNECTING:
+                // add code here
+                break;
+            case BluetoothProfile.STATE_CONNECTED:
+                // add code here
+                break;
+        }
+    }
+
+    private BluetoothAdapter.LeScanCallback mScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice bluetoothDevice, int i, final byte[] bytes) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // Find the board (type "unknown") by its appearance (add code).
+                }
+            });
+        }
+    };
+
+    private BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, final int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.i(TAG, "Connected to GATT server.");
+                if (mGatt.discoverServices()) {
+                    Log.i(TAG, "Started service discovery.");
+                } else {
+                    Log.w(TAG, "Service discovery failed.");
+                }
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.i(TAG, "Disconnected from GATT server.");
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    updateConnectButton(newState);
+                }
+            });
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
